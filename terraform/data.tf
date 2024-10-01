03515ae73c1fcdd87ced3178dd287899839dc761
trinami <eric@gmx.net>
2024-10-01 19:03:40 +0200

    fix: ns exists

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,3 +5,15 @@ data "sops_file" "secrets" {
 data "external" "env" {
   program = ["${path.module}/env.sh"]
 }
+
+data "kubernetes_namespace" "cert_manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+data "kubernetes_namespace" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+}
