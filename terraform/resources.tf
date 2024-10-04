5861f6c5bc0b23b3821b47147ed508b960cd5eb9
trinami <eric@gmx.net>
2024-10-04 22:34:09 +0200

    fix: deprecated value (visual studio extension needs update? not recognizing)

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -2,7 +2,7 @@ resource "cloudflare_record" "org_root_a" {
   depends_on = [module.kube-hetzner]
   zone_id = data.sops_file.secrets.data["trinami_org_zone_id"]
   name    = "@"
-  value   = module.kube-hetzner.ingress_public_ipv4
+  content   = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   proxied = false
   allow_overwrite = true
@@ -12,7 +12,7 @@ resource "cloudflare_record" "org_root_aaaa" {
   depends_on = [module.kube-hetzner]
   zone_id = data.sops_file.secrets.data["trinami_org_zone_id"]
   name    = "@"
-  value   = module.kube-hetzner.ingress_public_ipv6
+  content   = module.kube-hetzner.ingress_public_ipv6
   type    = "AAAA"
   proxied = false
   allow_overwrite = true
@@ -22,7 +22,7 @@ resource "cloudflare_record" "org_wildcard_a" {
   depends_on = [module.kube-hetzner]
   zone_id = data.sops_file.secrets.data["trinami_org_zone_id"]
   name    = "*"
-  value   = module.kube-hetzner.ingress_public_ipv4
+  content   = module.kube-hetzner.ingress_public_ipv4
   type    = "A"
   proxied = false
   allow_overwrite = true
@@ -32,7 +32,7 @@ resource "cloudflare_record" "org_wildcard_aaaa" {
   depends_on = [module.kube-hetzner]
   zone_id = data.sops_file.secrets.data["trinami_org_zone_id"]
   name    = "*"
-  value   = module.kube-hetzner.ingress_public_ipv6
+  content   = module.kube-hetzner.ingress_public_ipv6
   type    = "AAAA"
   proxied = false
   allow_overwrite = true
